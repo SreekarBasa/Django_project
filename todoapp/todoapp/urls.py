7a35319fa5f04c994afdb94ad34c24b7a46e6238
SREEKAR BASA <103530571+SreekarBasa@users.noreply.github.com>
2023-07-08 19:10:07 +0530

    change to normal

--- todoapp/todoapp/urls.py
+++ todoapp/todoapp/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from todolist import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='Todolist'),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
